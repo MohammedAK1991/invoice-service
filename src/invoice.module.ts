965d89ba63ecd28e252cc38a9219c3b549032d15
mohammedAK1991 <mohmed.ak.17@gmail.com>
2024-10-01 16:26:38 +0200

    refactor: Update invoice.module.ts imports and providers

--- src/invoice.module.ts
+++ src/invoice.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { InvoiceService } from './application/services/invoice/invoice.service';
+import { PubSubModule } from './common/pubsub/pubsub.module';
+import { MongoDBInvoiceRepository } from './infrastructure/repositories/mongodb-invoice.repository';
 import { Invoice, InvoiceSchema } from './domain/entities/invoice.entity';
+import { InvoiceService } from './application/services/invoice/invoice.service';
+import { InvoiceHandler } from './application/handlers/invoice.handler';
 import { InvoiceController } from './infrastructure/controllers/invoice/invoice.controller';
+import { PubSubService } from './common/pubsub/pubsub.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Invoice.name, schema: InvoiceSchema }]),
+    PubSubModule,
   ],
   controllers: [InvoiceController],
-  providers: [InvoiceService],
+  providers: [
+    InvoiceHandler,
+    InvoiceService,
+    PubSubService,
+    {
+      provide: 'IInvoiceRepository',
+      useClass: MongoDBInvoiceRepository,
+    },
+  ],
 })
 export class InvoiceModule {}
